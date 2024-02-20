6d17fbae9b3c24ce865c982de07de79cc98b7272
Santiago Stellatelli <sstellatelli@gmail.com>
2024-02-20 01:38:40 -0300

    pruebas 1

---
+++ javascript prueba/practicar.js
@@ -0,0 +1,38 @@
+//  const tempcel = Number(prompt('Ingresa una temperatura en ºC centígrados'));
+
+//  tempkelvin = `${tempcel+273}`
+
+//  console.log(`La temperatura es ${tempkelvin}`)
+
+ /*-------------------------------------------------*/
+//  var dividir = prompt('Ingrese un número que será el divisor');
+//  var numero = prompt(`Ingrese un número para dividir por ${dividir}`);
+
+
+//  const resultado ='El resultado es '+ parseInt( (numero)/ (dividir));
+
+//  console.log(resultado)
+/*------------------------------------------------------------------------*/
+
+// let a = [1,2,3];
+// let tipo = typeof(a[0])
+// alert(`Es un tipo ${tipo}`)
+
+/*----------------------------------------------------------------------------*/
+
+let numeros = [6,7,8,9,5,4,3,2,1,0]
+
+let num6 = numeros[0]
+let num7 = numeros[1]
+let num8 = numeros[2]
+let num9 = numeros[3]
+let num5 = numeros[4]
+let num4 = numeros[5]
+let num3 = numeros[6]
+let num2 = numeros[7]
+let num1 = numeros[8]
+let num0 = numeros[9]
+
+
+function calcu(){
+}
